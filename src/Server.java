be0a7d5eb98a3983648fb5bc4278ca0e30e03795
Arav PP <aravpp03@gmail.com>
2025-05-13 16:37:26 +0530

    Server frame added

--- src/Server.java
+++ src/Server.java
@@ -1,4 +1,33 @@
+import java.awt.Color;
 
-public class Server {
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class Server extends JFrame{
+	
+	public Server() {
+		
+		setLayout(null);
+		
+		JPanel p1 = new JPanel();
+		p1.setBackground(new Color(7,94 ,84));
+		p1.setBounds(0, 0, 450, 70);
+		add(p1);
+
+		
+		setSize(450 , 650);
+		setLocation(200 , 50);
+		getContentPane().setBackground(Color.white);
+		
+		
+		setVisible(true);
+		 
+	}
+	
+	public static void main(String[] args) {
+		
+		new Server();
+		
+	}
 
 }
