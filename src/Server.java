daa7293884f21b833d3d8242a85be6a1b6e7055d
Arav PP <aravpp03@gmail.com>
2025-05-13 20:11:43 +0530

    server back icon added

--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,9 @@
 import java.awt.Color;
+import java.awt.Image;
 
+import javax.swing.ImageIcon;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Server extends JFrame{
@@ -12,7 +15,15 @@ public class Server extends JFrame{
 		JPanel p1 = new JPanel();
 		p1.setBackground(new Color(7,94 ,84));
 		p1.setBounds(0, 0, 450, 70);
+		p1.setLayout(null);
 		add(p1);
+		
+		ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/3.png"));
+		Image i2 = i1.getImage().getScaledInstance(25, 25, Image.SCALE_DEFAULT);
+		ImageIcon i3 = new ImageIcon(i2); 
+		JLabel back = new JLabel(i3);
+		back.setBounds(5, 20, 25, 25);
+		p1.add(back);
 
 		
 		setSize(450 , 650);
