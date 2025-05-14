fbbbea005b6b4938a7e286bb708eb0eb826e7304
Arav PP <aravpp03@gmail.com>
2025-05-15 00:16:54 +0530

    Server modified

--- src/Server.java
+++ src/Server.java
@@ -7,6 +7,8 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
+import java.io.IOException;
+import java.net.ServerSocket;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
@@ -120,6 +122,17 @@ public class Server extends JFrame implements ActionListener{
 	public static void main(String[] args) {
 		
 		new Server();
+		
+		try {
+			ServerSocket skt = new ServerSocket(6001);
+			while(true) {
+				
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 		
 	}
 
